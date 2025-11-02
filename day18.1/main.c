25755abda05406ccaa1fa524147c44ea8091b366
sanaa <Sanaakumar07@gmail.com>
2025-11-02 14:54:13 +0530

    Initial Commit

---
+++ day18.1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  day18.1
+//
+//  Created by Sanaa Kumar on 02/11/25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
